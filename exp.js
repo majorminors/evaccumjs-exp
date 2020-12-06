ec5c216c81b5a771570a13d8d53f4de7f7e16b08
dorian <dorian@mino.rs>
2020-12-06 17:49:10 +0000

    add fixation with blank interval

--- exp.js
+++ exp.js
@@ -8,10 +8,11 @@ function make_experiment(id_number,images_only) {
         /////////////////
 
         var instructions_on = 1; // if 1, will do instructions
-        var dots_fixation = 1; // if 0 just a fixation, if 1 a fixation with dots
+        var dots_fixation = 2; // if 0 just a fixation, if 1 a fixation with dots
         var num_prac_trials = 5;
         var num_prac_blocks = 1; // I don't think this really does anything yet but it's coded in, so just leave it
-        var iti_range = [300,800]; // enter array of two values = [max, min]
+        var iti_range = [400,600]; // enter array of two values = [max, min]
+        var iti_duration = 300;
 
         // note both index.html and jatos.html expect to call a script 'tools/credentials.js' with a variable containing credential information for the axios requests, otherwise will need to include that here
 
@@ -148,6 +149,50 @@ function make_experiment(id_number,images_only) {
                         move_distance: 2.5, // I've only approximated the MATLAB experiment here - that's 5 degrees per second (like .01 Hz/fps) this is in pixel lengths per second...
                         dot_life: 7, // this is not the same as MATLAB - expressed in same units (frames of life), but MATLAB's 5 is visibly different to jsPsych's 5...
                         data: {experiment_part: 'fixation'}
+                    }
+                } else if (dots_fixation == 2 ) {
+                    var fixation = {
+                        timeline: [
+                            { // rdk with invisible dots
+                                type: 'rdk',
+                                background_color: "black",
+                                dot_color: "black", 
+                                aperture_type: 1,
+                                fixation_cross: true,
+                                fixation_cross_color: "white", 
+                                fixation_cross_thickness: 6,
+                                post_trial_gap: 0, 
+                                choices: jsPsych.NO_KEYS,
+                                response_ends_trial: false,
+                                correct_choice: "q",
+                                trial_duration: iti_duration,
+                                data: {experiment_part: 'fixation'}
+                            },
+                            { // rdk with random dots
+                                type: 'rdk',
+                                background_color: "black",
+                                dot_color: "white", 
+                                aperture_type: 1,
+                                fixation_cross: true,
+                                fixation_cross_color: "lightpink", 
+                                fixation_cross_thickness: 6,
+                                post_trial_gap: 0, 
+                                choices: jsPsych.NO_KEYS,
+                                response_ends_trial: false,
+                                correct_choice: "q",
+                                trial_duration: function(){ // lil function to randomise the iti between a range
+                                    var min = iti_range[0];
+                                    var max = iti_range[1];
+                                    var random_iti = Math.random() * (max - min) + min;
+                                    return random_iti;
+                                },
+                                number_of_dots: 100,
+                                coherence: 0, 
+                                move_distance: 2.5, // I've only approximated the MATLAB experiment here - that's 5 degrees per second (like .01 Hz/fps) this is in pixel lengths per second...
+                                dot_life: 7, // this is not the same as MATLAB - expressed in same units (frames of life), but MATLAB's 5 is visibly different to jsPsych's 5...
+                                data: {experiment_part: 'fixation'}
+                            }
+                        ]
                     }
                 }
  
